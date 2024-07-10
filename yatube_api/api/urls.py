02f3c18e084112fcdb647127e12a6934769bf807
Tania Belyashnikova <belyashnikova.tn@gmail.com>
2024-07-10 10:43:36 +0500

    change urls for JWT

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -12,6 +12,6 @@ router.register('groups', GroupViewSet)
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
 ]
